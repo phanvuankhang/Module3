03644c2a0534844898237d129f1319f698a3f599
Your Name <phanvuankhang@gmail.com>
2023-04-11 23:25:33 +0700

    up

--- case_study/case_study.sql
+++ case_study/case_study.sql
@@ -53,6 +53,40 @@ insert into nhan_vien value(1,"Nguyễn Văn An","1970-11-07","456231786",100000
 						   (10,"Nguyễn Công Đạo","1994-01-08","755434343",8000000,"0988767111","nguyencongdao12@gmail.com","6 Hoà Khánh, Đồng Nai",2,3,2);
 select * from nhan_vien;
 
+create table loai_khach(
+ma_loai_khach int primary key,
+ten_loai_khach varchar(50)
+);
+insert into loai_khach value(1,"Diamond"),
+							(2,"Platinium"),
+                            (3,"Gold"),
+                            (4,"Silver"),
+                            (5,"Member");
+select *from loai_khach;
+
+create table khach_hang(
+ma_khach_hang int primary key,
+ho_ten varchar(45),
+ngay_sinh date,
+gioi_tinh bit(1),
+so_cmnd varchar(45),
+so_dien_thoai varchar(45),
+email varchar(45),
+dia_chi varchar(45),
+ma_loai_khach int, foreign key(ma_loai_khach)references loai_khach(ma_loai_khach)
+);
+insert into khach_hang value (1,"Nguyễn Thị Hào","1970-11-07",0,"643431213","0945423362","thihao07@gmail.com","23 Nguyễn Hoàng, Đà Nẵng",5),
+							 (2,"Phạm Xuân Diệu","1992-08-08",1,"865342123","0954333333","xuandieu92@gmail.com","K77/22 Thái Phiên, Quảng Trị",3),
+                             (3,"Trương Đình Nghệ","1990-02-27",1,"488645199","0373213122","nghenhan2702@gmail.com","K323/12 Ông Ích Khiêm, Vinh",1),
+                             (4,"Dương Văn Quan","1981-07-08",1,"543432111","0490039241","duongquan@gmail.com","K453/12 Lê Lợi, Đà Nẵng",1),
+                             (5,"Hoàng Trần Nhi Nhi","1995-12-09",0,"795453345","0312345678","nhinhi123@gmail.com","224 Lý Thái Tổ, Gia Lai",4),
+                             (6,"Tôn Nữ Mộc Châu","2005-12-06",0,"732434215","0988888844","tonnuchau@gmail.com","37 Yên Thế, Đà Nẵng",4),
+                             (7,"Nguyễn Mỹ Kim","1984-04-08",0,"856453123","0912345698","kimcuong84@gmail.com","K123/45 Lê Lợi, Hồ Chí Minh",1),
+                             (8,"Nguyễn Thị Hào","1999-04-08",0,"965656433","0763212345","haohao99@gmail.com","55 Nguyễn Văn Linh, Kon Tu",3),
+                             (9,"Trần Đại Danh","1994-07-01",1,"432341235","0643343433","danhhhai99@gmail.com","24 Lý Thường Kiệt, Quảng Ngãi",1),
+                             (10,"Nguyễn Tâm Đắc","1989-07-01",1,"344343432","0987654321","dactam@gmail.com","22 Ngô Quyền, Đà Nẵng",2);
+select *from khach_hang;
+
 create table kieu_thue (
 ma_kieu_thue int primary key,
 ten_kieu_thue varchar(45)
@@ -86,12 +120,12 @@ dich_vu_mien_phi_di_kem varchar(45),
 ma_kieu_thue int, foreign key(ma_kieu_thue)references kieu_thue(ma_kieu_thue),
 ma_loai_dich_vu int ,foreign key(ma_loai_dich_vu)references loai_dich_vu(ma_loai_dich_vu)
 );
-insert into dich_vu value (1,'Villa Beach Front',25000,1000000,10,'vip','Có hồ bơi',500,4,null,3,1),
-						  (2,'House Princess 01',14000,5000000,7,'vip','Có thêm bếp nướng',null,3,null,2,2),
-						  (3,'Room Twin 01',5000,1000000,2,'normal','Có tivi',null,null,'1 Xe máy, 1 Xe đạp',4,3),
-						  (4,'Villa No Beach Front',22000,9000000,8,'normal','Có hồ bơi',300,3,null,3,1),
-						  (5,'House Princess 02',10000,4000000,5,'normal','Có thêm bếp nướng',null,2,null,3,2),
-						  (6,'Room Twin 02',3000,900000,2,'normal','Có tivi',null,null,'1 Xe máy',4,3);
+insert into dich_vu value (1,"Villa Beach Front",25000,1000000,10,"vip","Có hồ bơi",500,4,null,3,1),
+						  (2,"House Princess 01",14000,5000000,7,"vip","Có thêm bếp nướng",null,3,null,2,2),
+						  (3,"Room Twin 01",5000,1000000,2,"normal","Có tivi",null,null,"1 Xe máy, 1 Xe đạp",4,3),
+						  (4,"Villa No Beach Front",22000,9000000,8,"normal","Có hồ bơi",300,3,null,3,1),
+						  (5,"House Princess 02",10000,4000000,5,"normal","Có thêm bếp nướng",null,2,null,3,2),
+						  (6,"Room Twin 02",3000,900000,2,"normal","Có tivi",null,null,"1 Xe máy",4,3);
 select * from dich_vu;
 
 create table dich_vu_di_kem(
