db4e944530b47029b2173a89631353ddf0e671f3
Your Name <phanvuankhang@gmail.com>
2023-04-12 09:50:23 +0700

    up

--- ss1_relational_database/bai_tap/manage_student.sql
+++ ss1_relational_database/bai_tap/manage_student.sql
@@ -1,29 +1,29 @@
  CREATE DATABASE manage_student;
     USE manage_student;
     CREATE TABLE student(
-            id INT primary key,
+            id INT PRIMARY KEY,
 `name` VARCHAR(50)
 );
     CREATE TABLE teacher(
-            id INT primary key,
+            id INT PRIMARY KEY,
 `name` VARCHAR(50),
     age INT,
     country VARCHAR(50)
 );
 -- thêm dữ liệu
-    INSERT INTO student value (1,"Khang"),
+    INSERT INTO student VALUES (1,"Khang"),
 						  (2,"An");
-    INSERT INTO teacher value (1,"Chanh",35,"Vietnam"),
+    INSERT INTO teacher VALUES (1,"Chanh",35,"Vietnam"),
 						  (2,"Son",29,"Vietnam");
 -- hiển thị
     SELECT * FROM student;
     SELECT * FROM teacher;
 -- chỉnh sửa dữ liệu
-    UPDATE student SET name="Khanh" where id=2;
+    UPDATE student SET name="Khanh" WHERE id=2;
 -- xóa dữ liệu
-    DELETE FROM student where id=2;
+    DELETE FROM student WHERE id=2;
 -- ddl=> thêm cột poINT
-    ALTER TABLE student add column `poINT` INT;
+    ALTER TABLE student ADD COLUMN `point` INT;
 -- xóa bảng
     DROP TABLE student;
     DROP TABLE teacher;
