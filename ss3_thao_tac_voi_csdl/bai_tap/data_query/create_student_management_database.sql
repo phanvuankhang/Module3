a19a28a178e4245020dff0f2c11a2255ed321c45
Your Name <phanvuankhang@gmail.com>
2023-04-13 18:09:55 +0700

    up

---
+++ ss3_thao_tac_voi_csdl/bai_tap/data_query/create_student_management_database.sql
@@ -0,0 +1,33 @@
+CREATE DATABASE student_management;
+USE student_management;
+CREATE TABLE class(
+class_id INT PRIMARY KEY AUTO_INCREMENT,
+class_name VARCHAR(60) NOT NULL,
+star_date DATETIME NOT NULL,
+`status` BIT
+);
+CREATE TABLE student(
+student_id INT PRIMARY KEY AUTO_INCREMENT,
+student_name VARCHAR(30) NOT NULL,
+address VARCHAR(50),
+phone VARCHAR(20),
+`status` BIT,
+class_id INT NOT NULL,
+FOREIGN KEY (class_id) REFERENCES class(class_id)
+);
+CREATE TABLE `subject`(
+sub_id INT PRIMARY KEY AUTO_INCREMENT,
+sub_name VARCHAR(30) NOT NULL,
+credit TINYINT NOT NULL DEFAULT 1 CHECK (credit>=1),
+`status` BIT	
+);
+CREATE TABLE mark(
+mark_id INT PRIMARY KEY AUTO_INCREMENT,
+mark FLOAT DEFAULT 0 CHECK (mark BETWEEN 0 and 100),
+sub_id INT NOT NULL,
+student_id INT NOT NULL,
+exam_time INT NOT NULL,
+UNIQUE (sub_id,student_id),
+FOREIGN KEY (sub_id) REFERENCES `subject`(sub_id),
+FOREIGN KEY (student_id) REFERENCES student(student_id)
+);
---
+++ ss3_thao_tac_voi_csdl/bai_tap/data_query/query_data_with_student_management_database.sql
@@ -0,0 +1,18 @@
+USE student_management;
+-- Hiển thị tất cả các sinh viên có tên bắt đầu bảng ký tự ‘h’
+SELECT * FROM student
+WHERE student_name LIKE '%h%';
+-- Hiển thị các thông tin lớp học có thời gian bắt đầu vào tháng 12.
+SELECT * FROM class
+WHERE month(star_date)='12';
+-- Hiển thị tất cả các thông tin môn học có credit trong khoảng từ 3-5.
+SELECT * FROM  `subject`
+WHERE credit >=3 AND credit<=5;
+-- Thay đổi mã lớp(ClassID) của sinh viên có tên ‘Hung’ là 2.
+SET SQL_SAFE_UPDATES =0;
+UPDATE student SET class_id=2 WHERE student_name='Hung';
+SET SQL_SAFE_UPDATES =1;
+-- Hiển thị các thông tin: StudentName, SubName, Mark. Dữ liệu sắp xếp theo điểm thi (mark) giảm dần. nếu trùng sắp theo tên tăng dần.
+SELECT student.student_name, `subject`.sub_name, mark.mark
+FROM (student JOIN (mark JOIN `subject` ON mark.sub_id = `subject`.sub_id) ON mark.student_id = student.student_id)
+ORDER BY mark DESC, student_name;
