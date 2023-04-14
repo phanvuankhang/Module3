5465f7778739b5a2e4ff7d1fb12e7dfdd9b8be60
Your Name <phanvuankhang@gmail.com>
2023-04-14 22:10:15 +0700

    up

--- ss4_functions_in_sql/bai_tap/practice_common_functions.sql
+++ ss4_functions_in_sql/bai_tap/practice_common_functions.sql
@@ -6,11 +6,11 @@ WHERE credit=(SELECT MAX(credit) FROM `subject`);
 -- Hiển thị các thông tin môn học có điểm thi lớn nhất.
 SELECT s.* , m.mark AS max_point
 FROM `subject` s
-JOIN mark m ON s.sub_id=m.sub_id
+INNER JOIN mark m ON s.sub_id=m.sub_id
 WHERE m.mark=(SELECT MAX(m.mark) FROM mark m);
 
 -- Hiển thị các thông tin sinh viên và điểm trung bình của mỗi sinh viên, xếp hạng theo thứ tự điểm giảm dần
 SELECT s.*, AVG(m.mark) AS avg_mark
 FROM student s
-JOIN mark m ON s.student_id=m.student_id
+INNER JOIN mark m ON s.student_id=m.student_id
 GROUP BY student_id;
