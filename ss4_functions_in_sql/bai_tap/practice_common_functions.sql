fff59cb35326ce23ebeccbef63cb66217dca58b4
Your Name <phanvuankhang@gmail.com>
2023-04-17 09:07:16 +0700

    up

--- ss4_functions_in_sql/bai_tap/practice_common_functions.sql
+++ ss4_functions_in_sql/bai_tap/practice_common_functions.sql
@@ -7,7 +7,7 @@ WHERE credit=(SELECT MAX(credit) FROM `subject`);
 SELECT s.* , m.mark AS max_point
 FROM `subject` s
 INNER JOIN marks m ON s.sub_id=m.sub_id
-WHERE m.mark=(SELECT MAX(m.mark) FROM marks);
+WHERE m.mark=(SELECT MAX(m.mark) FROM marks m);
 
 -- Hiển thị các thông tin sinh viên và điểm trung bình của mỗi sinh viên, xếp hạng theo thứ tự điểm giảm dần
 SELECT s.*, AVG(m.mark) AS avg_mark
