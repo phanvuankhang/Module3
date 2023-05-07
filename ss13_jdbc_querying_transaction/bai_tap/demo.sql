820dd91c027e4ff2e9ad80085d784b18127b05fa
Your Name <phanvuankhang@gmail.com>
2023-05-07 15:46:40 +0700

    up

--- ss13_jdbc_querying_transaction/bai_tap/demo.sql
+++ ss13_jdbc_querying_transaction/bai_tap/demo.sql
@@ -2,7 +2,7 @@ CREATE DATABASE demo;
 USE demo;
 
 create table users (
- id  int(3) NOT NULL AUTO_INCREMENT,
+ id  int(3) AUTO_INCREMENT,
  name varchar(120) NOT NULL,
  email varchar(220) NOT NULL,
  country varchar(120),
